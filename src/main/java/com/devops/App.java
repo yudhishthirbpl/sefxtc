d4ef88e23e376dbca089df6768f6d1051d67463b
Yudhishthir Kaushik <yudhishthirbpl@hotmail.com>
2017-12-26 23:43:59 +0000

    initial code

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,11 @@
+package com.devops;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("--- This is just a sample app class for executing test code ---");
+
+	}
+
+}
---
+++ src/test/java/com/devops/testsuites/suite01/MatchPageTitle.java
@@ -0,0 +1,5 @@
+package com.devops.testsuites.suite01;
+
+public class MatchPageTitle {
+
+}
